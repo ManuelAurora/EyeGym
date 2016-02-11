cf29e414eca70070bde47e88a447c8278c5eec1b
Manuel <naruto06@mail.ru>
2016-02-11 15:23:27 +0300

    Simplified storyboard, basic functional added

--- EyeGym/TrainingViewController.swift
+++ EyeGym/TrainingViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class TrainingViewController: UIViewController
 {
     
-    @IBOutlet weak var _stack: UIStackView!
-    @IBOutlet weak var _startButton: UIButton!
-    @IBOutlet weak var _timeIntervalSegControl: UISegmentedControl!
-    @IBOutlet weak var _chooseTimeIntLabel: UILabel!
-    @IBOutlet weak var _pressStartLabel: UILabel!
-    
+    @IBOutlet weak var _startButton            : UIButton!
+    @IBOutlet weak var _chooseTimeIntLabel     : UILabel!
+    @IBOutlet weak var _pressStartLabel        : UILabel!
+    @IBOutlet weak var _timeIntervalSegControl : UISegmentedControl!
+   
+    var _leftCircle = UIImageView()
+    var _rightCircle = UIImageView()
     var _trainingInProcess = false
     
     @IBAction func _buttonTapped(sender: UIButton) {
@@ -40,7 +41,7 @@ class TrainingViewController: UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createCircles()
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,28 +51,72 @@ class TrainingViewController: UIViewController
 
 
     func performStretching(timer: NSTimeInterval) {
-        
-        _stack.spacing = 350
-        
+       
+        _leftCircle.leftAnchor.constraintEqualToAnchor(view.leftAnchor, constant: 30).active = true
+        _rightCircle.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor, constant: -30).active = true
+       
         UIView.animateWithDuration(timer, animations: {_ in
             self.view.layoutIfNeeded()
+           
+            }, completion: {_ in
+                self.removeCircles()
         })
     }
     
     func startStopTraining() {
+        
+        if _trainingInProcess {
+            view.layer.removeAllAnimations()
+            removeCircles()
+        }
+        
         _trainingInProcess = !_trainingInProcess
         
-        UIView.animateWithDuration(1.0, animations: {_ in
+        animateView()        
+    }
+    
+    func createCircles() {
+        view.layoutIfNeeded()
+        let leftCircle = UIImageView()
+        leftCircle.translatesAutoresizingMaskIntoConstraints = false
+        leftCircle.image = UIImage(named: "circle")
+        view.addSubview(leftCircle)
+        _leftCircle = leftCircle
+
+        leftCircle.leftAnchor.constraintEqualToAnchor(view.leftAnchor, constant: 180).active = true
+        leftCircle.topAnchor.constraintEqualToAnchor(view.topAnchor, constant: 60).active = true
+        leftCircle.heightAnchor.constraintEqualToConstant(70).active = true
+        leftCircle.widthAnchor.constraintEqualToConstant(70).active = true
+        
+        let rightCircle = UIImageView()
+        rightCircle.translatesAutoresizingMaskIntoConstraints = false
+        rightCircle.image = UIImage(named: "circle")
+        view.addSubview(rightCircle)
+        _rightCircle = rightCircle
+        
+        rightCircle.trailingAnchor.constraintGreaterThanOrEqualToAnchor(view.trailingAnchor, constant: -180).active = true
+        rightCircle.topAnchor.constraintEqualToAnchor(view.topAnchor, constant: 60).active = true
+        rightCircle.heightAnchor.constraintEqualToConstant(70).active = true
+        rightCircle.widthAnchor.constraintEqualToConstant(70).active = true        
+    }
+    
+    func removeCircles() {
+        _leftCircle.removeFromSuperview()
+        _rightCircle.removeFromSuperview()
+    }
+    
+    func animateView() {
+        
+        UIView.animateWithDuration(0.67, animations: {_ in
+            self.view.layoutIfNeeded()
             self._timeIntervalSegControl.hidden = self._trainingInProcess
             self._chooseTimeIntLabel.hidden = self._trainingInProcess
             self._pressStartLabel.hidden = self._trainingInProcess
-            self._trainingInProcess == true ? self._startButton.setTitle("Stop", forState: .Normal) : self._startButton.setTitle("Start", forState: .Normal)
+            }, completion: {_ in
+                    if self._trainingInProcess == false { self.createCircles() }
         })
         
-        if _trainingInProcess {
-            
-        }
-        
+        _trainingInProcess == true ? _startButton.setTitle("Stop", forState: .Normal) : self._startButton.setTitle("Start", forState: .Normal)
     }
     
 }
