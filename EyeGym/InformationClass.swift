50bcdec6d19eee9cb12190d85d804ff927e0af3f
Manuel <naruto06@mail.ru>
2016-10-12 17:31:24 +0300

    Added: New Assets && Refactored

--- EyeGym/InformationClass.swift
+++ EyeGym/InformationClass.swift
@@ -9,37 +9,7 @@
 import UIKit
 
 struct Information {
-    var _Images = [UIImage]()
-    var _Text   = [String]()
     
-    init(button: String) {
-       _Images = chooseImages(button)
-       _Text = chooseText(button)
-    }
-    
-    func chooseImages(kind: String) -> [UIImage]{
-        
-        switch kind
-        {
-        case "about" : []
-        case "info" : []
-        default : break
-        }
-        
-       return [UIImage(named: "circle")!]
-    }
-    
-    func chooseText(kind: String) -> [String]{
-        switch kind
-        {
-        case "about" : []
-        case "info" : []
-        default : break
-        }
-        
-        return [""]
-    }
-    
-    let aboutText = [""]
-    let infoText = [""]
+
+
 }
--- EyeGym/TrainingViewController.swift
+++ EyeGym/TrainingViewController.swift
@@ -11,8 +11,16 @@ import UIKit
 class TrainingViewController: UIViewController
 {
     
-    @IBOutlet weak var rightImage:              UIImageView!
-    @IBOutlet weak var leftImage:               UIImageView!
+    @IBOutlet weak var rightImage:              UIImageView! {
+        didSet {
+            prepare(image: rightImage)
+        }
+    }
+    @IBOutlet weak var leftImage:               UIImageView! {
+        didSet {
+            prepare(image: leftImage)
+        }
+    }
     @IBOutlet weak var chooseTimeIntLabel:      UILabel!
     @IBOutlet weak var pressStartLabel:         UILabel!
     @IBOutlet weak var rotateNotificationLabel: UILabel!
@@ -23,51 +31,82 @@ class TrainingViewController: UIViewController
     
     enum Direction
     {
-        case Left
-        case Right
+        case left
+        case right
     }
     
-    var started: Bool = false
+    var isStarted = false {
+        didSet {
+            toggleButtonTitle()
+            toggleAnimations()
+        }
+    }
     
     @IBAction func start() {
+        let timer = calculateTime(for: timeIntervalSegControl)
+            
+        animate(leftImage, with: timer, direction: .left)
+        animate(rightImage, with: timer, direction: .right)
         
-        
-        
-        animateImage(leftImage, time: Double(60), direction: .Left)
-        animateImage(rightImage, time: Double(60), direction: .Right)
-        
-        started = true
+        isStarted = !isStarted
     }
     
-    func animateImage(image: UIImageView, time: Double, direction: Direction) {
+    func animate(_ image: UIImageView, with time: Double, direction: Direction) {
         
         let leftOriginalCenter  = leftImage.center
         let rightOriginalCenter = rightImage.center
       
         let movePic = CABasicAnimation(keyPath: "position.x")
         
-        movePic.fromValue = direction == .Left ? leftOriginalCenter.x : rightOriginalCenter.x
-        movePic.toValue   = direction == .Left ? (0 + image.frame.width / 2) : (view!.bounds.width - image.frame.width / 2)
-        movePic.duration  = Double(time)
-        //movePic.fillMode  = kCAFillModeBoth
+        movePic.fromValue = direction == .left ? leftOriginalCenter.x : rightOriginalCenter.x
+        movePic.toValue   = direction == .left ? (image.frame.width / 2) : (view!.bounds.width - image.frame.width / 2)
+        movePic.duration  = time
         
-        //movePic.removedOnCompletion = false
-        
-        image.layer.addAnimation(movePic, forKey: nil)
+        image.layer.add(movePic, forKey: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        leftImage.layer.cornerRadius   = 30
-        rightImage.layer.cornerRadius  = 30
         
-        leftImage.layer.masksToBounds  = true
-        rightImage.layer.masksToBounds = true
+    }
+    
+    func prepare(image view: UIImageView) {
         
+        view.layer.cornerRadius  = 30
+        view.layer.masksToBounds = true
+    }
+    
+    func toggleButtonTitle() {
+        
+        let title =  isStarted ? "Stop" : "Start"
+
+        startButton.setTitle(title, for: .normal)
+    }
+    
+    func toggleAnimations() {
+        
+        if !isStarted
+        {
+            leftImage.layer.removeAllAnimations()
+            rightImage.layer.removeAllAnimations()
         }
+    }
     
-    
+    func calculateTime(for segmentedControl: UISegmentedControl) -> Double {
+        
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            return 60.0
+        case 1:
+            return 90.0
+        case 2:
+            return 120.0
+        default:
+            return 60.0
+        }
+    }
 }
 
 
